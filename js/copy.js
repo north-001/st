aa860634c734e48334986a5546e97d2429742671
north-001 <u001e002s003@gmail.com>
2025-11-06 16:33:26 -0500

    Add files via upload

--- js/copy.js
+++ js/copy.js
@@ -1,10 +1,14 @@
-      const copyBtn = document.getElementById("copyBtn");
-      const textToCopy = document.getElementById("textToCopy");
+  const copyBtn = document.getElementById("copyLocation");
+  const textToCopy = document.getElementById("textCopyLocation");
 
-      copyBtn.addEventListener("click", () => {
-        // Используем современный API clipboard
-        navigator.clipboard
-          .writeText(textToCopy.textContent)
-          .then(() => {})
-          .catch((err) => {});
+  copyBtn.addEventListener("click", () => {
+    const text = textToCopy.textContent.trim(); // берём текст из <p>
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log("Скопировано:", text);
+      })
+      .catch((err) => {
+        console.error("Ошибка копирования:", err);
       });
+  });
