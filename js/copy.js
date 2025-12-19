6a85b6f5a27d4195bbc58963958a3b50ea3bc83d
north-001 <u001e002s003@gmail.com>
2025-12-19 04:43:21 -0500

    Add files via upload

--- js/copy.js
+++ js/copy.js
@@ -12,3 +12,14 @@ copyBtn.addEventListener("click", () => {
       //console.error("--", err);
     });
 });
+
+document.querySelectorAll(".copy-location").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const text = btn
+      .closest("div")
+      .querySelector(".text-copy-location")
+      .textContent.trim();
+
+    navigator.clipboard.writeText(text);
+  });
+});
